341b9561814e711519555071c07ab54d0d6438b7
Guilherme Alves <guii05@hotmail.com>
2020-08-05 18:10:44 -0300

    Filtro da requisição melhorado.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:async/async.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=b2116ce3";
 
 void main() async{
 
   http.Response response = await http.get(request); //await faz ficar esperando a resposta
-  print(response.body);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
 
   runApp(MaterialApp(
     home: Container()
